3fdd4bd342614c3ee7ef056e91ae8e34e839580c
miyaxu0312 <39933311+miyaxu0312@users.noreply.github.com>
2018-07-25 16:09:47 +0800

    Update util.hpp

--- util.hpp
+++ util.hpp
@@ -26,13 +26,13 @@ namespace Landmark{
         Mat affine_mat;
         Mat crop_img;
     };
-    void pre_process(const string ImagePath, const string boxPath, const string netOutPath, const string postPath, const string uv_kpt_ind, const string faceIndex, const string savePath, int resolution, vector<Affine_Matrix> &affine_matrix);
-    void post_process(const string ori_path, const string filePath, const string save_path, const string pose_save, const string canonical_vertices, const string faceIndex, const string uv_kpt_ind, int resolution, vector<Affine_Matrix> &affine_matrix, const string plot_path);
+    void pre_process(const string ImagePath, const string boxPath, const string netOutPath, const string postPath, const string uv_kpt_ind, const string faceIndex, const string savePath, int resolution, vector<Affine_Matrix> &affine_matrix, string suffix);
+    void post_process(const string ori_path, const string filePath, const string save_path, const string pose_save, const string canonical_vertices, const string faceIndex, const string uv_kpt_ind, int resolution, vector<Affine_Matrix> &affine_matrix, const string plot_path, string suffix);
     vector<string> get_all_files(string path, string suffix);
     vector<string> my_split(string my_str, string seperate);
     bool searchkey(vector<int> a, int value);
     void getFromText(String nameStr, Mat &myMat);
-    vector<int> get_box(string path, string name);
+    vector<int> get_box(string path, string name, string suffix);
     void plot_landmark(Mat image, string name, vector<vector<float>> kpt, string plot_path);
     vector<vector<float>> get_vertices(Mat pos, vector<float> face_ind, int resolution);
     vector<vector<float>> get_landmark(Mat pos, vector<float> uv_kpt_ind_0, vector<float> uv_kpt_ind_1);
