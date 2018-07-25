691be86f95e220cf2b81083cd3293431189f8f1f
miyaxu0312 <39933311+miyaxu0312@users.noreply.github.com>
2018-07-25 16:09:09 +0800

    Update util.cpp

--- util.cpp
+++ util.cpp
@@ -29,12 +29,11 @@ using namespace std;
 using namespace cv;
 
 namespace Landmark {
-void pre_process(string filePath, string boxPath, string netOutPath, string postPath, string uv_kpt_ind,string faceIndex, string savePath, int resolution, vector<Affine_Matrix> &affine_matrix)
+void pre_process(string filePath, string boxPath, string netOutPath, string postPath, string uv_kpt_ind,string faceIndex, string savePath, int resolution, vector<Affine_Matrix> &affine_matrix, string suffix)
 {
     vector<string> files;
     vector<string> split_result;
     vector<int> box;
-    string  suffix = ".*.jpg";
     Mat img;
     files = get_all_files(filePath, suffix);
     cout<<"-----image num:-----"<<files.size()<<endl;
@@ -84,7 +83,7 @@ void pre_process(string filePath, string boxPath, string netOutPath, string post
     }
 }
 
-void post_process(string ori_path, string filePath, string save_path, string pose_save, string canonical_vertices_path, string faceIndex, string uv_kpt_ind_path, int resolution, vector<Affine_Matrix> &affine_matrix,string plot_path)
+void post_process(string ori_path, string filePath, string save_path, string pose_save, string canonical_vertices_path, string faceIndex, string uv_kpt_ind_path, int resolution, vector<Affine_Matrix> &affine_matrix,string plot_path, string suffix)
 {
     vector<string> files;
     vector<string> split_result;
@@ -92,11 +91,10 @@ void post_process(string ori_path, string filePath, string save_path, string pos
     Mat img, ori_img, z, vertices_T, stacked_vertices, affine_mat_stack;
     Mat pos(resolution,resolution,CV_8UC3);
     string name;
-    string  suffix = ".*.jpg";
     files = get_all_files(filePath, suffix);
     for(int i=0; i<files.size(); ++i)
     {
-        string tmp;
+        string tmp = "";
         bool isfind = false;
         img = imread(files[i], IMREAD_UNCHANGED); // 读取每一张图片
         split_result = my_split(files[i], "/");
@@ -117,7 +115,7 @@ void post_process(string ori_path, string filePath, string save_path, string pos
         ori_img = imread(ori_path + "/" + name, IMREAD_UNCHANGED);    //加载原始图片，方便画landmark
         Mat cropped_vertices(resolution*resolution,3,img.type()), cropped_vertices_T(3,resolution*resolution,img.type());
         
-        cropped_vertices = img.reshape(1, resolution*resolution);
+        cropped_vertices = img.reshape(1, resolution * resolution);
         Mat cropped_vertices_swap(resolution*resolution,3,cropped_vertices.type());
         
         cropped_vertices.col(0).copyTo(cropped_vertices_swap.col(2));
@@ -141,9 +139,9 @@ void post_process(string ori_path, string filePath, string save_path, string pos
         pos = vertices_T.reshape(3,resolution);
         Mat pos2(resolution,resolution,CV_64FC3);
         
-        for (int row=0; row<pos.rows;++row)
+        for (int row = 0; row < pos.rows; ++row)
         {
-            for (int col=0; col<pos.cols;++col)
+            for (int col = 0; col < pos.cols; ++col)
             {
                 pos2.at<Vec3d>(row,col)[0] = pos.at<Vec3d>(row,col)[2];
                 pos2.at<Vec3d>(row,col)[1] = pos.at<Vec3d>(row,col)[1];
@@ -294,13 +292,12 @@ void getFromText(String nameStr, Mat &myMat)
             iss >> num;
             myMat.at<float>(line,i)=num;
         }
-        ++line;
-        
+        ++line;    
     }
     myFaceFile.close();
 }
 
-vector<int> get_box(string path, string name)
+vector<int> get_box(string path, string name, string suffix)
 {
     vector<string> box;
     vector<int> box_int;
@@ -309,7 +306,7 @@ vector<int> get_box(string path, string name)
     string line;               //保存读入的每一行
     while(getline(f,line))
     {
-        if (line+".jpg" == name)
+        if (line + suffix == name)
         {
             getline(f,line);
             box = my_split(line,",");
@@ -325,159 +322,154 @@ vector<int> get_box(string path, string name)
     }
     return box_int;
 }
-    
-   
-    
-    void plot_landmark(Mat img, string name, vector<vector<float>> kpt, string plot_path)
-    {
-        Mat image = img.clone();
-        vector<int> end_list = {17-1, 22-1, 27-1, 42-1, 48-1, 31-1, 36-1, 68-1};
-        for(int i=0;i<68;i++)
-        {
-            int start_point_x, start_point_y, end_point_x, end_point_y;
-            start_point_x = int(round(kpt[i][0]));
-            start_point_y = int(round(kpt[i][1]));
-            Point center1(start_point_x,start_point_y);
-            circle(image, center1, 2, Scalar(0,0,255));
-            if (searchkey(end_list,i))
-                continue;
-            end_point_x = int(round(kpt[i+1][0]));
-            end_point_y = int(round(kpt[i+1][1]));
-            Point center2(end_point_x,end_point_y);
-            line(image, center1, center2, Scalar(0,255,0));
-        }
        
-        if (access(plot_path.c_str(),6)==-1)
-        {
-            mkdir(plot_path.c_str(), S_IRWXU);
-        }
-        imwrite(plot_path + "/" + name, image);
-        cout<<"-----plot-kpt-completed-----"<<endl;
-    }
-    
-    vector<vector<float>> get_vertices(Mat pos, vector<float> face_ind, int resolution)
+void plot_landmark(Mat img, string name, vector<vector<float>> kpt, string plot_path)
+{
+    Mat image = img.clone();
+    vector<int> end_list = {17-1, 22-1, 27-1, 42-1, 48-1, 31-1, 36-1, 68-1};
+    for(int i = 0; i < 68; i++)
     {
-        Mat all_vertices = pos.reshape(1,resolution*resolution);
-        vector<vector<float>> result(face_ind.size(),vector<float>(3,0));
-        vector<float>::iterator iter;
-        int i=0;
-        for (iter=face_ind.begin();iter!=face_ind.end();iter++)
-        {
-            result[i][0] = all_vertices.at<double>(int(*iter),2);
-            result[i][1] = all_vertices.at<double>(int(*iter),1);
-            result[i][2] = all_vertices.at<double>(int(*iter),0);
-            i++;
-        }
+        int start_point_x, start_point_y, end_point_x, end_point_y;
+        start_point_x = int(round(kpt[i][0]));
+        start_point_y = int(round(kpt[i][1]));
+        Point center1(start_point_x,start_point_y);
+        circle(image, center1, 2, Scalar(0,0,255));
         
-        return result;
+        if (searchkey(end_list,i))
+            continue;
+        
+        end_point_x = int(round(kpt[i+1][0]));
+        end_point_y = int(round(kpt[i+1][1]));
+        Point center2(end_point_x,end_point_y);
+        line(image, center1, center2, Scalar(0,255,0));
     }
-    
-    vector<vector<float>> get_landmark(Mat pos, vector<float> uv_kpt_ind_0,vector<float> uv_kpt_ind_1)
-    {
-        vector<vector<float>> landmark(68,vector<float>(3,0));
-        for (int i=0; i<uv_kpt_ind_0.size();++i)
-        {
-            landmark[i][0] = pos.at<Vec3d>(uv_kpt_ind_1[i],uv_kpt_ind_0[i])[2];
-            landmark[i][1] = pos.at<Vec3d>(uv_kpt_ind_1[i],uv_kpt_ind_0[i])[1];
-            landmark[i][2] = pos.at<Vec3d>(uv_kpt_ind_1[i],uv_kpt_ind_0[i])[0];
-        }
-        
-        return landmark;
-    }
-    
-    vector<float> estimate_pose(vector<vector<float>> vertices, string canonical_vertices_path)
-    {
-        Mat canonical_vertices_homo;
-        Mat canonical_vertices = Mat::zeros(131601/3, 3, CV_32FC1);
-        getFromText(canonical_vertices_path, canonical_vertices);
-        
-        Mat ones_mat(131601/3,1,canonical_vertices.type(),Scalar(1));
-        ones_mat.convertTo(ones_mat, CV_32F);
-        hconcat(canonical_vertices, ones_mat, canonical_vertices_homo);
-        
-        Mat canonical_vertices_homo_T, vertices_T;
-        CvMat *canonical_vertices_homo_T_pointer=cvCreateMat(43867, 4, CV_32FC1);
-        CvMat *vertices_T_pointer=cvCreateMat(43867, 3, CV_32FC1);
-        CvMat *P_pointer=cvCreateMat(4, 3,CV_32FC1);
-        
-        cvSetData(canonical_vertices_homo_T_pointer, canonical_vertices_homo.data, CV_AUTOSTEP);
-        
-        for (int i = 0; i < 43867; i++)
-        {
-            for(int j = 0; j < 3; j++)
-            {
-                cvmSet(vertices_T_pointer, i, j, vertices[i][j]);
-            }
-        }
-        
-        cvSolve(canonical_vertices_homo_T_pointer,vertices_T_pointer, P_pointer);
-        
-        Mat P(P_pointer->rows,P_pointer->cols,P_pointer->type,P_pointer->data.fl);
-        Mat P_T;
-        transpose(P, P_T);
-        
-        Mat rotation_matrix = P2sRt(P_T);
-        cout<<"-------P2sRt completed-------"<<endl;
-        vector<float> pose = matrix2angle(rotation_matrix);
-        cout<<"-------matrix2angle completed-------"<<endl;
-        
-        return pose;
-    }
-    
-    //p 3*4
-    Mat P2sRt(Mat P)
-    {
-        Mat t2d(2,1, P.type());
-        Mat R1(1,3, P.type());
-        Mat R2(1,3, P.type());
-        Mat R(3,3, P.type());
-        //t2d
-        Mat P_row0 = P.rowRange(0,1).clone();
-        R1 = P_row0.colRange(0, 3).clone();
-        Mat P_row1 = P.row(1).clone();
-        P_row1.colRange(0, 3).copyTo(R2);
-        Mat r1 = R1 / norm(R1);
-        Mat r2 = R2 / norm(R2);
-        
-        CvMat *r1_pointer=cvCreateMat(1, 3,CV_32FC1);
-        cvSetData(r1_pointer, r1.data, CV_AUTOSTEP);
-        
-        CvMat *r2_pointer=cvCreateMat(1, 3,CV_32FC1);
-        cvSetData(r2_pointer, r2.data, CV_AUTOSTEP);
-        
-        CvMat *r3_pointer=cvCreateMat(1, 3,CV_32FC1);
-        cvCrossProduct(r1_pointer,r2_pointer,r3_pointer);
-        
-        Mat r3(r3_pointer->rows,r3_pointer->cols, r3_pointer->type, r3_pointer->data.fl);
-        vconcat(r1, r2, R);
-        vconcat(R, r3, R);
-        return R;
-    }
-    
-    //r 3*3
-    vector<float> matrix2angle(Mat R)
-    {
-        vector<float> pose_angle;
-        float x,y,z;
-        if (R.at<float>(2,0) != 1 || R.at<float>(2,0) != -1)
-        {
-            x = asin(R.at<float>(2,0));
-            y = atan2(R.at<float>(2,1)/cos(x), R.at<float>(2,2)/cos(x));
-            z = atan2(R.at<float>(1,0)/cos(x), R.at<float>(0,0)/cos(x));
-        }
-        else{
-            z = 0;
-            if (R.at<float>(2,0) == -1)
-            {
-                x = M_PI / 2;
-                y = z + atan2(-R.at<float>(0,1), -R.at<float>(0,2));
-            }
-        }
-        pose_angle.push_back(x);
-        pose_angle.push_back(y);
-        pose_angle.push_back(z);
-        return pose_angle;
-    }
-    
 
+    if (access(plot_path.c_str(),6)==-1)
+    {
+        mkdir(plot_path.c_str(), S_IRWXU);
+    }
+    imwrite(plot_path + "/" + name, image);
+}
+    
+vector<vector<float>> get_vertices(Mat pos, vector<float> face_ind, int resolution)
+{
+    Mat all_vertices = pos.reshape(1,resolution*resolution);
+    vector<vector<float>> result(face_ind.size(),vector<float>(3,0));
+    vector<float>::iterator iter;
+    int i=0;
+    for (iter=face_ind.begin();iter!=face_ind.end();iter++)
+    {
+        result[i][0] = all_vertices.at<double>(int(*iter),2);
+        result[i][1] = all_vertices.at<double>(int(*iter),1);
+        result[i][2] = all_vertices.at<double>(int(*iter),0);
+        i++;
+    }
+
+    return result;
+}
+    
+vector<vector<float>> get_landmark(Mat pos, vector<float> uv_kpt_ind_0,vector<float> uv_kpt_ind_1)
+{
+    vector<vector<float>> landmark(68,vector<float>(3,0));
+    for (int i=0; i<uv_kpt_ind_0.size();++i)
+    {
+        landmark[i][0] = pos.at<Vec3d>(uv_kpt_ind_1[i],uv_kpt_ind_0[i])[2];
+        landmark[i][1] = pos.at<Vec3d>(uv_kpt_ind_1[i],uv_kpt_ind_0[i])[1];
+        landmark[i][2] = pos.at<Vec3d>(uv_kpt_ind_1[i],uv_kpt_ind_0[i])[0];
+    }
+
+    return landmark;
+}
+    
+vector<float> estimate_pose(vector<vector<float>> vertices, string canonical_vertices_path)
+{
+    Mat canonical_vertices_homo;
+    Mat canonical_vertices = Mat::zeros(131601/3, 3, CV_32FC1);
+    getFromText(canonical_vertices_path, canonical_vertices);
+
+    Mat ones_mat(131601/3,1,canonical_vertices.type(),Scalar(1));
+    ones_mat.convertTo(ones_mat, CV_32F);
+    hconcat(canonical_vertices, ones_mat, canonical_vertices_homo);
+
+    Mat canonical_vertices_homo_T, vertices_T;
+    CvMat *canonical_vertices_homo_T_pointer = cvCreateMat(43867, 4, CV_32FC1);
+    CvMat *vertices_T_pointer = cvCreateMat(43867, 3, CV_32FC1);
+    CvMat *P_pointer = cvCreateMat(4, 3,CV_32FC1);
+
+    cvSetData(canonical_vertices_homo_T_pointer, canonical_vertices_homo.data, CV_AUTOSTEP);
+
+    for (int i = 0; i < 43867; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            cvmSet(vertices_T_pointer, i, j, vertices[i][j]);
+        }
+    }
+
+    cvSolve(canonical_vertices_homo_T_pointer,vertices_T_pointer, P_pointer);
+
+    Mat P(P_pointer->rows,P_pointer->cols,P_pointer->type,P_pointer->data.fl);
+    Mat P_T(P_pointer->cols,P_pointer->rows,P_pointer->type);
+    
+    transpose(P, P_T);
+
+    Mat rotation_matrix = P2sRt(P_T);
+    vector<float> pose = matrix2angle(rotation_matrix);
+    return pose;
+}
+
+//p 3*4
+Mat P2sRt(Mat P)
+{
+    Mat t2d(2,1, P.type());
+    Mat R1(1,3, P.type());
+    Mat R2(1,3, P.type());
+    Mat R(3,3, P.type());
+    //t2d
+    Mat P_row0 = P.rowRange(0,1).clone();
+    R1 = P_row0.colRange(0, 3).clone();
+    Mat P_row1 = P.row(1).clone();
+    P_row1.colRange(0, 3).copyTo(R2);
+    Mat r1 = R1 / norm(R1);
+    Mat r2 = R2 / norm(R2);
+
+    CvMat *r1_pointer=cvCreateMat(1, 3,CV_32FC1);
+    cvSetData(r1_pointer, r1.data, CV_AUTOSTEP);
+
+    CvMat *r2_pointer=cvCreateMat(1, 3,CV_32FC1);
+    cvSetData(r2_pointer, r2.data, CV_AUTOSTEP);
+
+    CvMat *r3_pointer=cvCreateMat(1, 3,CV_32FC1);
+    cvCrossProduct(r1_pointer,r2_pointer,r3_pointer);
+
+    Mat r3(r3_pointer->rows,r3_pointer->cols, r3_pointer->type, r3_pointer->data.fl);
+    vconcat(r1, r2, R);
+    vconcat(R, r3, R);
+    return R;
+}
+
+//r 3*3
+vector<float> matrix2angle(Mat R)
+{
+    vector<float> pose_angle(3,1);
+    float x = 0, y = 0, z = 0;
+    if (R.at<float>(2,0) != 1 || R.at<float>(2,0) != -1)
+    {
+        x = asin(R.at<float>(2,0));
+        y = atan2(R.at<float>(2,1)/cos(x), R.at<float>(2,2)/cos(x));
+        z = atan2(R.at<float>(1,0)/cos(x), R.at<float>(0,0)/cos(x));
+    }
+    else{
+        z = 0;
+        if (R.at<float>(2,0) == -1)
+        {
+            x = M_PI / 2;
+            y = z + atan2(-R.at<float>(0,1), -R.at<float>(0,2));
+        }
+    }
+    pose_angle.push_back(x);
+    pose_angle.push_back(y);
+    pose_angle.push_back(z);
+    return pose_angle;
+}
 }
